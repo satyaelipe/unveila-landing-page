ef3525d8514818298acd70a7d945fedb68786553
Satya Elipe <39649784+satyaelipe@users.noreply.github.com>
2025-03-23 15:55:31 +0000

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LandingPage from "./LandingPage";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<LandingPage />);
