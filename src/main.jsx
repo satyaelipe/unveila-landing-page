beb135d208e3df9cbd30c4433f65e47602c2006d
Satya Elipe <39649784+satyaelipe@users.noreply.github.com>
2025-03-23 18:58:13 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import LandingPage from "./LandingPage";
+import "../styles/globals.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<LandingPage />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LandingPage />
+  </React.StrictMode>
+);
