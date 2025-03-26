2a36b242dbab5a39a12afb5af4a89f7fba5fd193
Satya Elipe <39649784+satyaelipe@users.noreply.github.com>
2025-03-26 16:15:28 +0000

    Update LandingPage.jsx

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -23,7 +23,8 @@ export default function LandingPage() {
     if (hash) {
       const el = document.querySelector(hash);
       if (el) {
-        const yOffset = -96; // adjust based on your header height
+        const isHome = hash === "#home";
+        const yOffset = isHome ? 0 : -96;
         const y = el.getBoundingClientRect().top + window.pageYOffset + yOffset;
         window.scrollTo({ top: y, behavior: 'smooth' });
       }
